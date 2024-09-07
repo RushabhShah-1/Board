28cd80328125dc258cf73cf6d8a217d3feea5fd9
Rushabh <141929113+RushabhShah-1@users.noreply.github.com>
2024-09-07 10:32:12 +0530

    Update auth.config.js

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://optimum-pheasant-56.clerk.accounts.dev",
+      domain: "https://rushabhshah.live",
       applicationID: "convex",
     },
   ],
